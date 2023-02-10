9f65511c8cb24b5e775a49d6d77ac257394c011a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-02-10 12:01:23 +0200

    Update script.js
    Functionality of displaying the balance has been added.

--- script.js
+++ script.js
@@ -3,27 +3,36 @@ class Bank {
     this.balance = balance;
   }
   withdraw(amount) {
-    if(this.balance - amount <= 0){
-        console.log('You can not withdraw more than what you have!')
-        console.log({balance: this.balance})
-        return
+    if (this.balance - amount <= 0) {
+      console.log("You can not withdraw more than what you have!");
+      console.log({ balance: this.balance });
+      return;
     }
     this.balance -= amount;
-    console.log('withdraw', `$${amount}`)
-    console.log({balance: this.balance})
+    console.log("withdraw", `$${amount}`);
+    console.log({ balance: this.balance });
   }
   deposit(amount) {
     this.balance += amount;
-    console.log('deposited', `$${amount}`)
-    console.log({balance: this.balance})
+    console.log("deposited", `$${amount}`);
+    console.log({ balance: this.balance });
   }
 }
-const userChecking = new Bank(200)
-console.log(userChecking.balance) 
+const userChecking = new Bank(200);
+console.log(userChecking.balance);
 
-const depositButton = document.getElementById('deposit')
-const withdrawButton = document.getElementById('withdraw')
-const amountInput = document.getElementById('amount')
+const depositButton = document.getElementById("deposit");
+const withdrawButton = document.getElementById("withdraw");
+const amountInput = document.getElementById("amount");
+const balanceDiv = document.getElementById("balance");
 
-depositButton.onclick = () => userChecking.deposit(Number(amountInput.value))
-withdrawButton.onclick = () => userChecking.withdraw(Number(amountInput.value))
+depositButton.onclick = () => {
+  const amount = Number(amountInput.value);
+  userChecking.deposit(Number(amountInput.value));
+  balanceDiv.innerText = `Balance: ${userChecking.balance}`;
+};
+withdrawButton.onclick = () => {
+  const amount = Number(amountInput.value);
+  userChecking.withdraw(amount);
+  balanceDiv.innerText = `Balance: ${userChecking.balance}`;
+};
