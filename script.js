a4cdd0d2a635cd97fb8558eb37fee0065757f213
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-02-10 11:53:18 +0200

    update script.js
    Button functionality of withdraw or deposit is up and live.

--- script.js
+++ script.js
@@ -3,10 +3,27 @@ class Bank {
     this.balance = balance;
   }
   withdraw(amount) {
+    if(this.balance - amount <= 0){
+        console.log('You can not withdraw more than what you have!')
+        console.log({balance: this.balance})
+        return
+    }
     this.balance -= amount;
+    console.log('withdraw', `$${amount}`)
+    console.log({balance: this.balance})
   }
   deposit(amount) {
     this.balance += amount;
+    console.log('deposited', `$${amount}`)
+    console.log({balance: this.balance})
   }
 }
-const userChecking = new Bank(100)
+const userChecking = new Bank(200)
+console.log(userChecking.balance) 
+
+const depositButton = document.getElementById('deposit')
+const withdrawButton = document.getElementById('withdraw')
+const amountInput = document.getElementById('amount')
+
+depositButton.onclick = () => userChecking.deposit(Number(amountInput.value))
+withdrawButton.onclick = () => userChecking.withdraw(Number(amountInput.value))
