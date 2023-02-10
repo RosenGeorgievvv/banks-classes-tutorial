01358897b55c676ae1776703068211d2a15c3bb9
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-02-10 10:56:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-class Bank{
-    
+class Bank {
+  constructor(balance) {
+    this.balance = balance;
+  }
+  withdraw(amount) {
+    this.balance -= amount;
+  }
+  deposit(amount) {
+    this.balance += amount;
+  }
 }
+const userChecking = new Bank(100)
